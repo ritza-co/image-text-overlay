cc58a937c096048dee3afe39b2ef4465e2e236e9
AnesuMurakata <murac96@gmail.com>
2022-06-24 09:15:08 +0200

    bug fixes

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ from rq import Worker, Queue, Connection
 listen = ['high', 'default', 'low']
 
 if __name__ == "__main__":
-    app.run()
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
+    app.run()
+    
